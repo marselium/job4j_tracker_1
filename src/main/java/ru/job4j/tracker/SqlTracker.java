72a6e0c8a17c440ece2f0b881b4dc80a41432e0b
Marsel Yumagulov <marseliumagulov@yandex.ru>
2026-06-09 22:55:34 +0300

    2. Liquibase. Добавление новых скриптов. [#504893 #466343]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -19,10 +19,9 @@ public class SqlTracker implements Store {
     }
 
     private void init() {
-        try (InputStream input = SqlTracker.class.getClassLoader()
-                .getResourceAsStream("app.properties")) {
+        try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream("db/liquibase.properties")) {
             Properties config = new Properties();
-            config.load(input);
+            config.load(in);
             Class.forName(config.getProperty("driver-class-name"));
             connection = DriverManager.getConnection(
                     config.getProperty("url"),
